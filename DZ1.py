45236d67b34c179c063dad306ab3a29ed0c3b018
unknown <braytkratts@bk.ru>
2023-04-16 23:02:25 +1000

    начал решать

---
+++ DZ1.py
@@ -0,0 +1,26 @@
+# k = int(input('Видите кол-во элементов в строке: '))
+# m_list = []
+# number = 0
+# for i in range(k):
+#     r = int(input('Видите элемент : '))
+#     m_list.append(r)
+# print(m_list)
+# x = int(input('Видите элемент x: '))
+# for j in range(k):
+#     if m_list(j) == x:
+#         number += number
+# print(number)
+
+
+# N = abs(int(input('Введите количество элементов списка А: ')))
+# A_entered = input("Введите через пробел элементы списка: ").split()
+# A_num = list(map(int, A_entered))
+# if len(A_num) != N:
+#     print('Введенные элементы не соответствуют заявленному количеству!')
+# else:
+#     X = int(input('Введите число X, которое необходимо найти в списке: '))
+#     count = 0
+#     for i in range(N):
+#         if A_num[i] == X:
+#             count += 1
+#     print(f'Число {X} встречается в списке A {count} раз')
