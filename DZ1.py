bacfc5ff1e494f064c509cbc7bbdcfdd4f0fe879
unknown <braytkratts@bk.ru>
2023-04-18 21:22:28 +1000

    решение первой задачи

--- DZ1.py
+++ DZ1.py
@@ -1,15 +1,15 @@
-# k = int(input('Видите кол-во элементов в строке: '))
-# m_list = []
-# number = 0
-# for i in range(k):
-#     r = int(input('Видите элемент : '))
-#     m_list.append(r)
-# print(m_list)
-# x = int(input('Видите элемент x: '))
-# for j in range(k):
-#     if m_list(j) == x:
-#         number += number
-# print(number)
+k = int(input('Видите кол-во элементов в строке: '))
+m_list = []
+number = 0
+for i in range(k):
+    r = int(input('Видите элемент : '))
+    m_list.append(r)
+print(m_list)
+x = int(input('Видите элемент x: '))
+for j in range(k):
+    if m_list[j] == x:
+        number += 1
+print(number)
 
 
 # N = abs(int(input('Введите количество элементов списка А: ')))
